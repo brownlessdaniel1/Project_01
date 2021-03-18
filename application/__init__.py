909fa1f077d1e52bb23f0ba3e7659a7b0835854c
brownlessdaniel1 <DBrownless@qa.com>
2021-03-18 19:11:37 +0000

    db: compatability issues; resetting vm

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:rooot@34.89.40.226df/project_01"
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:rooot@34.89.40.226/project_01"
 app.config["SECRET_KEY"]="SECRET_KEY"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
