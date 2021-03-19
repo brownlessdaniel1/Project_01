0ea3b0e77468b4ba80530b1a628931cafadf5cf4
brownlessdaniel1 <DBrownless@qa.com>
2021-03-19 20:38:18 +0000

    db: added run.sh for jenkins integration, attempt 7

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update -y
 sudo apt install python3-pip
+sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
 sudo apt install gunicorn
-gunicorn --workers=2 --bind=0.0.0.0:5000 app:app
+gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
