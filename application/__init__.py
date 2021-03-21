e4723b7a739cd607da64d182d862313d5077eb2e
brownlessdaniel1 <DBrownless@qa.com>
2021-03-21 22:37:24 +0000

    final touches_p3

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:rooot@35.230.133.171/project_01"
 # app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///test-db"
 app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("SQL_URI")
 app.config["SECRET_KEY"]="SECRET_KEY"
