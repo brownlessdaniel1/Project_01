f3f7cf948c05872381c521407bffcce553edf4d6
Ubuntu <ubuntu@ip-172-31-27-99.eu-west-2.compute.internal>
2021-07-22 08:33:41 +0000

    db: minor changes to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt-get update -y
+sudo apt-get update -y
+sudo apt-get install -y python3-venv python3-pip
+python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
 gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
